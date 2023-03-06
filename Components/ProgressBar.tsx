cd5fe49ff4e8c7450b0f5a096706205e914e0739
Srikar Parsi <srikar.parsi@expensify.com>
2023-03-05 23:04:16 -0500

    some ui changes

--- Components/ProgressBar.tsx
+++ Components/ProgressBar.tsx
@@ -9,11 +9,14 @@ type ProgressBarProps = {
   
 function ProgressBar(props: ProgressBarProps): JSX.Element {
     return (
-        <View style={{flexDirection: 'row', width:102, height:17, borderColor:'#3a3a3a', borderWidth:1, marginRight:10, borderRadius:4, overflow:'hidden'}}>
-            <View style={{width:25, height: 15, backgroundColor: '#e5e5e5'}} />
-            <View style={{width:25, height: 15, backgroundColor: '#c9c9c9'}} />
-            <View style={{width:25, height: 15, backgroundColor: '#aeaeae'}} />
-            <View style={{width:25, height: 15, backgroundColor: '#838383'}} />
+        <View style={{flexDirection: 'row', width:101, height:17, borderColor:'#3a3a3a', borderWidth:1, marginRight:10, borderRadius:4, overflow:'hidden'}}>
+            {props.step >= 1 && <View style={{width:33, height: 15, backgroundColor: '#e5e5e5'}} />}
+            {props.step >= 2 && <View style={{width:33, height: 15, backgroundColor: '#c9c9c9'}} />}
+            {props.step >= 3 &&<View style={{width:33, height: 15, backgroundColor: '#aeaeae'}} />}
+
+            {/* <View style={{width:33, height: 15, backgroundColor: '#c4b8fd'}} />
+            <View style={{width:33, height: 15, backgroundColor: '#b2a2fc'}} />
+            <View style={{width:33, height: 15, backgroundColor: '#a08dfb'}} />  */}
         </View>
     );
 }
